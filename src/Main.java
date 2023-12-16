b0bc3577ae5d5f1542df07d1e6802f305ddca076
Zaha Fatima zahra <fatimazahra.zaha1504@gmail.com>
2023-12-16 14:40:52 +0100

    edit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import GUI.InterfaceConnexion;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new InterfaceConnexion();
     }
 }
