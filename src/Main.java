8a8dacb67e3fca0ca2541a63fff9243c4f36ce9d
D0git <dohagit@gmail.com>
2023-12-17 00:55:45 +0100

    edit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
+
+import GUI.Abonnement;
 import GUI.InterfaceConnexion;
 import GUI.InterfaceInscription;
 public class Main {
     public static void main(String[] args) {
 
-        String title ="Maison Intelligente";
-       new InterfaceConnexion(title);
+        //String title ="Maison Intelligente";
+        //new InterfaceConnexion(title);
         //new InterfaceInscription(title);
+        //String title2 ="Abonnement";
+        //new Abonnement(title2);
     }
 }
